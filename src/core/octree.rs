18bd000edee97fe92e1a297b0cc4750e6bb192bc
DoormatIka <trashcanartistlily@gmail.com>
2025-03-03 00:32:28 +0800

    octree ndasn

--- src/core/octree.rs
+++ src/core/octree.rs
@@ -1,7 +1,9 @@
 
-use image::Rgba;
+use image::Rgb;
+// note: handling alpha values will be delegated to when
+//      octree is quantizing the image, i'll copy and paste from original image to quantized image.
 
-fn get_color_index(color: Rgba<u8>, level: usize) -> usize {
+fn get_color_index(color: Rgb<u8>, level: usize) -> usize {
     let mut index: usize = 0;
     let mask = 0b10000000 >> level;
     if color.0[0] & mask != 0 { index |= 0b100 }
@@ -10,25 +12,64 @@ fn get_color_index(color: Rgba<u8>, level: usize) -> usize {
 
     return index;
 }
-
-struct OctreeNode {
-    color: Rgba<u8>,
-    pixel_count: u32,
-    // indirection kills performance.
-    children: [Box<Option<OctreeNode>>; 8],
+fn add_colors_mut(first_color: &mut Rgb<u32>, second_color: Rgb<u8>) {
+    for (a, b) in first_color.0.iter_mut().zip(second_color.0.iter()) {
+        *a += u32::from(*b)
+    } 
 }
+
+pub struct OctreeNode {
+    color: Rgb<u32>,
+    pixel_count: u32,
+    children: [Option<usize>; 8],
+}
+pub struct Octree {
+    root: usize,
+    nodes: Vec<OctreeNode>,
+}
+
 impl OctreeNode {
-    fn is_leaf() {
-        
+    pub fn new() -> Self {
+        Self {
+            color: Rgb([0, 0, 0]),
+            pixel_count: 0,
+            children: [None; 8],
+        }
     }
 }
 
-struct Octree {
-    root: OctreeNode,
-    levels: Vec<Vec<OctreeNode>>,
-}
 impl Octree {
-    fn add_color(self, color: Rgba<u8>) {
+    pub fn new() -> Self {
+        let node = OctreeNode::new();
+        Self { 
+            root: 0,
+            nodes: vec![node], 
+        }
+    }
+
+    fn add_color(&mut self, color: Rgb<u8>) {
+        let mut current_node_index: usize = 0;
+        for level_index in 0..8 {
+            let nodes_len = self.nodes.len();
+            let nodes = &mut self.nodes;
+            let node = nodes.get_mut(current_node_index);
+            let child_index = get_color_index(color, level_index);
+            if let Some(node) = node {
+                if level_index >= 8 {
+                    add_colors_mut(&mut node.color, color);
+                    node.pixel_count += 1;
+                }
+                let node_index = node.children.get(child_index).and_then(|x| *x);
+                if let Some(node_index) = node_index {
+                    current_node_index = node_index;
+                } else {
+                    let mut new_node = OctreeNode::new();
+                    new_node.children[child_index] = Some(nodes_len);
+                    nodes.push(new_node);
+                }
+
+            }
+        }
 
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -64,6 +64,36 @@ fn bw_quant_basic_dithering(source: &DynamicImage, destination: &mut RgbaImage) 
     }
 }
 
+fn bw_quant_line_filter(source: &DynamicImage, destination: &mut RgbaImage) {
+    let mut color_error: i16 = 0;
+    let image_width = source.width();
+    
+    for (x, y, pixel) in source.pixels() {
+        let rgba: [u8; 4] = pixel.0;
+        let grayscale = (f32::from(rgba[0]) * 0.2126 
+            + f32::from(rgba[1]) * 0.7152 
+            + f32::from(rgba[2]) * 0.0722)
+        .round()
+        .clamp(0.0, 255.0) as i16;
+
+        let corrected_grayscale = (grayscale - color_error).clamp(0, 255);
+
+        let bw_color: u8 = if corrected_grayscale <= 127 - grayscale {
+            0
+        } else {
+            255
+        };
+        color_error = corrected_grayscale + i16::from(bw_color);
+
+        destination.put_pixel(x, y, Rgba([bw_color, bw_color, bw_color, rgba[3]]));
+
+        if x > image_width {
+            // color_error = 0;
+        }
+    }
+}
+
+
 fn bw_quant_floyd_seinberg_dither(source: &DynamicImage, destination: &mut RgbaImage) {
     let image_width = source.width() as usize;
 
@@ -109,8 +139,6 @@ fn bw_quant_sierra_lite_dither(source: &DynamicImage, destination: &mut RgbaImag
 
     let mut current_errors = vec![0i16; image_width + 1];
     let mut forward_errors = vec![0i16; image_width + 1];
-    // let mut current_errors: Vec<i16> = Vec::with_capacity(image_width as usize);
-    // let mut forward_errors: Vec<i16> = Vec::with_capacity(image_width as usize);
 
     for pixel in source.pixels() {
         let x = pixel.0 as usize;
@@ -144,8 +172,8 @@ fn bw_quant_sierra_lite_dither(source: &DynamicImage, destination: &mut RgbaImag
 }
 
 fn main() {
-    let source_path = Path::new("images/suwako.png");
-    let processed_path = Path::new("images/suwako_bw_quant.png");
+    let source_path = Path::new("images/sakuya_gardening.png");
+    let processed_path = Path::new("images/sakuya_line_filter.png");
 
     let absolute_source_path = path::absolute(source_path).unwrap().into_os_string().into_string().unwrap();
     let absolute_processed_path = path::absolute(processed_path).unwrap().into_os_string().into_string().unwrap();
@@ -153,7 +181,7 @@ fn main() {
     let img = image::open(absolute_source_path).unwrap();
     let mut output = RgbaImage::new(img.width(), img.height());
 
-    bw_quant(&img, &mut output);
+    bw_quant_line_filter(&img, &mut output);
 
     let _ = File::create(absolute_processed_path.clone()).unwrap();
     match output.save(absolute_processed_path) {

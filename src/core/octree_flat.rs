f5d62ad06e8067bff8e6b0a5752aadbe67a69bbf
DoormatIka <trashcanartistlily@gmail.com>
2025-03-24 12:46:18 +0800

    getcolorindex

--- src/core/octree_flat.rs
+++ src/core/octree_flat.rs
@@ -8,9 +8,9 @@ use image::Rgb;
 pub fn get_color_index(color: Rgb<u8>, level: usize) -> usize {
     let mut index: usize = 0;
     let mask = 0b10000000 >> level;
-    if color.0[0] & mask != 0 { index |= 0b100 }
-    if color.0[1] & mask != 0 { index |= 0b010 }
-    if color.0[2] & mask != 0 { index |= 0b001 }
+    if color.0[0] & mask != 0 { index |= 0b100; }
+    if color.0[1] & mask != 0 { index |= 0b010; }
+    if color.0[2] & mask != 0 { index |= 0b001; }
 
     return index;
 }
